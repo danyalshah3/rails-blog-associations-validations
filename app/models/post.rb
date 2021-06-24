4355ccaf781730fd8f5d366d9e51fc8cfb7e079c
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-06-24 10:43:25 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
     belongs_to :user
-    has_many :tags
+    has_many :post_tags
+    has_many :tags, through: :post_tags
 validates :name, :content, presence: true
 
   
